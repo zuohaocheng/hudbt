839a846bd57120df696d351d1906b30c8f64aeed
Eggsorer <laisum.sid@gmail.com>
2012-03-31 17:37:41 +0800

    Issue 98:1.freelech.php will now use post method instead of unsafe get. And the options had become several ugly buttons...

--- freeleech.php
+++ freeleech.php
@@ -5,38 +5,38 @@ loggedinorreturn();
 if (get_user_class() < UC_ADMINISTRATOR)
 	stderr("Error", "Permission denied.");
 
-$action = isset($_POST['action']) ? htmlspecialchars($_POST['action']) : (isset($_GET['action']) ? htmlspecialchars($_GET['action']) : 'main');
-if ($action == 'setallfree')
+$action = (isset($_POST['status']) ? $_POST['status'] : 'main');
+if ($action == 'Free')
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 2");
 	$Cache->delete_value('global_promotion_state');
 	stderr('Success','All torrents have been set free..');
 }
-elseif ($action == 'setall2up')
+elseif ($action == '2XUP')
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 3");
 	$Cache->delete_value('global_promotion_state');
 	stderr('Success','All torrents have been set 2x up..');
 }
-elseif ($action == 'setall2up_free')
+elseif ($action == '2XFree')
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 4");
 	$Cache->delete_value('global_promotion_state');
 	stderr('Success','All torrents have been set 2x up and free..');
 }
-elseif ($action == 'setallhalf_down')
+elseif ($action == '50%Down')
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 5");
 	$Cache->delete_value('global_promotion_state');
 	stderr('Success','All torrents have been set half down..');
 }
-elseif ($action == 'setall2up_half_down')
+elseif ($action == '2XUp&50%Down')
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 6");
 	$Cache->delete_value('global_promotion_state');
 	stderr('Success','All torrents have been set half down..');
 }
-elseif ($action == 'setallnormal') 
+elseif ($action == 'Normal') 
 {
 	sql_query("UPDATE torrents_state SET global_sp_state = 1");
 	$Cache->delete_value('global_promotion_state');
@@ -44,6 +44,18 @@ elseif ($action == 'setallnormal')
 }
 elseif ($action == 'main')
 {
-	stderr('Select action','Click <a class=altlink href=freeleech.php?action=setallfree>here</a> to set all torrents free.. <br /> Click <a class=altlink href=freeleech.php?action=setall2up>here</a> to set all torrents 2x up..<br /> Click <a class=altlink href=freeleech.php?action=setall2up_free>here</a> to set all torrents 2x up and free.. <br />Click <a class=altlink href=freeleech.php?action=setallhalf_down>here</a> to set all torrents half down..<br />Click <a class=altlink href=freeleech.php?action=setall2up_half_down>here</a> to set all torrents 2x up and half down..<br />Click <a class=altlink href=freeleech.php?action=setallnormal>here</a> to set all torrents normal..', false);
-}
+	//stderr('Select action','Click <a class=altlink href=freeleech.php?action=setallfree>here</a> to set all torrents free.. <br /> Click <a class=altlink href=freeleech.php?action=setall2up>here</a> to set all torrents 2x up..<br /> Click <a class=altlink href=freeleech.php?action=setall2up_free>here</a> to set all torrents 2x up and free.. <br />Click <a class=altlink href=freeleech.php?action=setallhalf_down>here</a> to set all torrents half down..<br />Click <a class=altlink href=freeleech.php?action=setall2up_half_down>here</a> to set all torrents 2x up and half down..<br />Click <a class=altlink href=freeleech.php?action=setallnormal>here</a> to set all torrents normal..', false);
+stdhead();
 ?>
+<div class="minor-list-vertical center"><ul>
+	<li><form method="post" ><input type="submit" value="Free" name="status" /></form></li>
+<li><form method="post" ><input type="submit" value="2XUP" name ="status" /></form></li>
+	<li><form method="post" ><input type="submit" value="2XFree" name="status"/></form></li>
+	<li><form method="post" ><input type="submit" value="50%Down" name="status" /></form></li>
+<li><form method="post" ><input type="submit" value="2XUp&50%Down" name="status"/></form></li>
+<li><form method="post" ><input type="submit" value="Normal" name="status"/></form></li>
+</ul></div>
+<?php
+stdfoot();
+}
+
